c8c477a5692ddf3ee40c66c204d40dc65a11b43e
Neo <neo.temores@live.com>
2024-01-09 09:47:22 -0800

    refactor view-all to import fetch & display funcs

--- index.js
+++ index.js
@@ -6,3 +6,41 @@ export async function fetchEmoji(url) {
     return data
 }
 
+export function displayCards (data, pageDiv, start, end){
+    pageDiv.innerHTML = "";
+    for(let i = start; i < end; i++) {
+        const card = document.createElement("div")
+        card.className = "emoji-card"
+        const emojiDiv = document.createElement("div")
+        emojiDiv.className = "emoji"
+        const unicodeDiv = document.createElement("div")
+        const nameDiv = document.createElement("div")
+        const categoryDiv = document.createElement("div")
+        const groupDiv = document.createElement("div")
+
+        let unicode = data[i].unicode[0]
+        let emoji = unicode.replace("U+", "&#x")
+        let name = data[i].name
+        let category = data[i].category
+        let group = data[i].group
+        
+        if(name.includes("≊")){
+            name = name.slice(0, name.indexOf("≊"))
+        }
+
+        emojiDiv.innerHTML = emoji
+        unicodeDiv.innerHTML = "Unicode: " + unicode
+        nameDiv.innerHTML = "Name: "+ name
+        categoryDiv.innerHTML = "Category: " + category
+        groupDiv.innerHTML = "Group: " + group
+        
+
+        card.appendChild(emojiDiv)
+        card.appendChild(unicodeDiv)
+        card.appendChild(nameDiv)
+        card.appendChild(categoryDiv)
+        card.appendChild(groupDiv)
+    
+        pageDiv.appendChild(card)
+    }  
+}
--- view-all.js
+++ view-all.js
@@ -1,32 +1,26 @@
-import {fetchEmoji} from "./index.js"
+import {fetchEmoji, displayCards} from "./index.js"
 
 let viewAllPageDiv= document.getElementById("view-all-page")
 //https://github.com/cheatsnake/emojihub
 let url = "https://emojihub.yurace.pro/api/all"
 
-const data = await fetchEmoji(url)
-
-let allEmojiArrr = [];
-let emojiArrLength = 0;
+const allEmojiArrr = await fetchEmoji(url)
+const numOfEmojiPerPage = 12;
 let currentPage = 1;
-let totalPages = 1;
-function storeData(data){
-    allEmojiArrr = data;
-    emojiArrLength = data.length
-    totalPages = Math.ceil(emojiArrLength / 12)
-    displayCards(allEmojiArrr)
-}
+let totalPages = Math.ceil(allEmojiArrr.length / numOfEmojiPerPage);
+
 
 let startIndex = 0;
-let endIndex = 12;
+let endIndex = numOfEmojiPerPage;
+
 const backBtn = document.querySelector(".back-btn")
 backBtn.addEventListener("click", ()=> {
-    paginate(-12)
+    paginate(-numOfEmojiPerPage)
 })
 
 const forwardBtn = document.querySelector(".forward-btn")
 forwardBtn.addEventListener("click", ()=> {
-    paginate(12)
+    paginate(numOfEmojiPerPage)
 })
 
 function paginate(value){
@@ -34,62 +28,21 @@ function paginate(value){
     endIndex += value
     currentPage = value > 0 ? currentPage+=1 : currentPage-=1
 
-    if(currentPage < 1 || currentPage > 150){
+    if(currentPage < 1 || currentPage > totalPages){
         currentPage = 1;
         startIndex = 0;
-        endIndex = 12;    
+        endIndex = numOfEmojiPerPage;    
     }
 
     updateCurrentPage()
-    displayCards(allEmojiArrr)
+    displayCards(allEmojiArrr, viewAllPageDiv, startIndex, endIndex)
 }
 
 const currentPageSpan = document.querySelector(".current-page")
 function updateCurrentPage() {
-    let text = `Page ${currentPage} of ${totalPages}`
-    currentPageSpan.innerHTML = text
+    let currPageText = `Page ${currentPage} of ${totalPages}`
+    currentPageSpan.innerHTML = currPageText
 }
 
-function displayCards (data){
-    updateCurrentPage()
-    viewAllPageDiv.innerHTML = "";
-    for(let i = startIndex; i < endIndex; i++) {
-        const card = document.createElement("div")
-        card.className = "emoji-card"
-        const emojiDiv = document.createElement("div")
-        emojiDiv.className = "emoji"
-        const unicodeDiv = document.createElement("div")
-        const nameDiv = document.createElement("div")
-        const categoryDiv = document.createElement("div")
-        const groupDiv = document.createElement("div")
-
-        let unicode = data[i].unicode[0]
-        let emoji = unicode.replace("U+", "&#x")
-        let name = data[i].name
-        let category = data[i].category
-        let group = data[i].group
-        
-        if(name.includes("≊")){
-            name = name.slice(0, name.indexOf("≊"))
-        }
-
-        emojiDiv.innerHTML = emoji
-        unicodeDiv.innerHTML = "Unicode: " + unicode
-        nameDiv.innerHTML = "Name: "+ name
-        categoryDiv.innerHTML = "Category: " + category
-        groupDiv.innerHTML = "Group: " + group
-        
-
-        card.appendChild(emojiDiv)
-        card.appendChild(unicodeDiv)
-        card.appendChild(nameDiv)
-        card.appendChild(categoryDiv)
-        card.appendChild(groupDiv)
-    
-        viewAllPageDiv.appendChild(card)
-    }
-   
-}
-
-
-storeData(data)
+updateCurrentPage()
+displayCards(allEmojiArrr, viewAllPageDiv, startIndex, endIndex)
