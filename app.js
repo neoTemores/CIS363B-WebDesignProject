2348f5563be112c96f8e451795bb69448a7bfcb9
Neo <neo.temores@live.com>
2024-01-13 23:05:32 -0800

    createCard() & random emoji mvp

--- app.js
+++ app.js
@@ -10,43 +10,47 @@ export function displayCards (data, pageDiv, start, end){
     pageDiv.innerHTML = "";
     let endIndex = end > data.length ? data.length : end
     for(let i = start; i < endIndex; i++) {
-        const card = document.createElement("div")
-        card.className = "emoji-card"
-        const emojiDiv = document.createElement("div")
-        emojiDiv.className = "emoji"
-        const unicodeDiv = document.createElement("div")
-        const nameDiv = document.createElement("div")
-        const categoryDiv = document.createElement("div")
-        const groupDiv = document.createElement("div")
-
-        let unicode = data[i].unicode[0]
-        let emoji = unicode.replace("U+", "&#x")
-        emoji += ";"
-        let name = data[i].name
-        let category = data[i].category
-        let group = data[i].group
-        
-        if(name.includes("≊")){
-            name = name.slice(0, name.indexOf("≊"))
-        }
-
-        emojiDiv.innerHTML = emoji
-        unicodeDiv.innerHTML = "Unicode: " + unicode
-        nameDiv.innerHTML = "Name: "+ name
-        categoryDiv.innerHTML = "Category: " + category
-        groupDiv.innerHTML = "Group: " + group
-        
-
-        card.appendChild(emojiDiv)
-        card.appendChild(unicodeDiv)
-        card.appendChild(nameDiv)
-        card.appendChild(categoryDiv)
-        card.appendChild(groupDiv)
-    
+        let card = createCard(data[i])
         pageDiv.appendChild(card)
     }  
 }
 
+export function createCard(dataObj) {
+    let card = document.createElement("div")
+    card.className = "emoji-card"
+    const emojiDiv = document.createElement("div")
+    emojiDiv.className = "emoji"
+    const unicodeDiv = document.createElement("div")
+    const nameDiv = document.createElement("div")
+    const categoryDiv = document.createElement("div")
+    const groupDiv = document.createElement("div")
+
+    let unicode = dataObj.unicode[0]
+    let emoji = unicode.replace("U+", "&#x")
+    emoji += ";"
+    let name = dataObj.name
+    let category = dataObj.category
+    let group = dataObj.group
+    
+    if(name.includes("≊")){
+        name = name.slice(0, name.indexOf("≊"))
+    }
+
+    emojiDiv.innerHTML = emoji
+    unicodeDiv.innerHTML = "Unicode: " + unicode
+    nameDiv.innerHTML = "Name: "+ name
+    categoryDiv.innerHTML = "Category: " + category
+    groupDiv.innerHTML = "Group: " + group
+    
+
+    card.appendChild(emojiDiv)
+    card.appendChild(unicodeDiv)
+    card.appendChild(nameDiv)
+    card.appendChild(categoryDiv)
+    card.appendChild(groupDiv)
+
+    return card;
+}
 
 export function paginate(emojiPerPage, start, end, currPage, totalPages, emojiArr, pageDiv, currPageDiv){
     start += emojiPerPage
--- src/js/test-unicode.js
+++ src/js/test-unicode.js
@@ -1,1 +1,27 @@
-import { fetchEmoji } from "../../app.js";
+import { fetchEmoji, createCard } from "../../app.js";
+
+loadTestUnicodePage();
+async function loadTestUnicodePage(){
+    let randomBtn = document.getElementById("random-emoji-btn");
+    randomBtn.addEventListener("click", displayRandomEmoji);
+}
+
+async function displayRandomEmoji(){
+    let randomEmojiDiv = document.getElementById("random-emoji")
+    randomEmojiDiv.innerHTML = "<i>Loading...</i>";
+    let url = "https://emojihub.yurace.pro/api/random"
+    let is200response;
+    let emojiObj;
+    let card;
+
+    try {
+        emojiObj = await fetchEmoji(url);
+        card = createCard(emojiObj)
+        is200response = true;
+    } catch (error) {
+        is200response = false;
+    }
+    
+    randomEmojiDiv.innerHTML = "";
+    is200response ? randomEmojiDiv.appendChild(card) : randomEmojiDiv.innerHTML = "<i>Error!</i>"
+}   
