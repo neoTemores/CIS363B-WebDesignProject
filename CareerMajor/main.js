7e75739ab7fe386c87f0b390accff7254b8241a0
Neo <neo.temores@live.com>
2024-01-31 10:23:02 -0800

    Home page done, nav bar working

--- CareerMajor/main.js
+++ CareerMajor/main.js
@@ -1,6 +1,21 @@
-loadPage();
-function loadPage() {
-  setCurrentDate();
+setCurrentDate();
+updateNavbar();
+
+function updateNavbar() {
+  let pathNameArr = window.location.pathname.toString().split("/");
+  let endPoint = pathNameArr[pathNameArr.length - 1];
+  let navbarLinks = document.querySelectorAll(".navbarLink");
+
+  navbarLinks.forEach((elem) => {
+    elem.classList.remove("activePage");
+    if (elem.id == endPoint) {
+      elem.classList.add("activePage");
+    }
+  });
+
+  if (endPoint == "") {
+    navbarLinks[0].classList.add("activePage");
+  }
 }
 
 function setCurrentDate() {
